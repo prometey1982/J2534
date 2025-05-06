eeedc7606d45cf60846ad57c66698dcb3d96b093
Artem Vasilev <prometey1982@mail.ru>
2025-05-06 12:04:24 +0300

    Added copy contructor and operator= for BaseMessage

--- j2534/BaseMessage.hpp
+++ j2534/BaseMessage.hpp
@@ -6,9 +6,18 @@ namespace j2534 {
 
 class BaseMessage {
 public:
+  BaseMessage(const BaseMessage&) = default;
+  BaseMessage(BaseMessage&&) = default;
   explicit BaseMessage(uint32_t canId)
     : _canId{ canId }
   {
+  }
+  virtual ~BaseMessage() {}
+
+  BaseMessage& operator=(const BaseMessage& rhs)
+  {
+    _canId = rhs._canId;
+    return *this;
   }
 
   virtual std::vector<PASSTHRU_MSG>
